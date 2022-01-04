ca6fd166f3621ba6b0af4fc994cb0f5ec9f6ff84
hyabc <yizetang@hotmail.com>
2022-01-04 17:05:44 -0500

    Add background image to homepage

--- homemode.c
+++ homemode.c
@@ -6,6 +6,7 @@ typedef struct{
     Device* dev;
     GLuint button;
 	GLuint active_button;
+	GLuint background;
 	size_t count;
 	size_t current;
 } HomemodeData;
@@ -24,6 +25,12 @@ int homemode_init(ESContext* esContext, State* state, Device* dev)
 	data->button = image_load(&dev->image, 1, 1, button_buffer, 3);
 	unsigned char active_button_buffer[3] = {128, 0, 0}; 
 	data->active_button = image_load(&dev->image, 1, 1, active_button_buffer, 3);
+
+	int bg_width, bg_height;
+	char* bg_buf = esLoadTGA(NULL, "bg.tga", &bg_width, &bg_height);
+	data->background = image_load(&dev->image, bg_width, bg_height, bg_buf, 3);
+	free(bg_buf);
+
 	data->count = 2;
 	data->current = 0;
     return 0;
@@ -33,6 +40,7 @@ void homemode_destroy(ESContext* esContext, State* state){
 	HomemodeData* data = state->data;
 	image_unload(&data->dev->image, data->button);
 	image_unload(&data->dev->image, data->active_button);
+	image_unload(&data->dev->image, data->background);
     free(state->data);
 }
 
@@ -82,6 +90,8 @@ void homemode_draw(ESContext* esContext, State* state)
 {   
     HomemodeData* data = state->data;
     glClear(GL_COLOR_BUFFER_BIT);
+
+	image_render(&data->dev->image, 0.0, 0.0, 1.0, 1.0, data->background, NULL);
     
     image_render(&data->dev->image, 0.35, 0.5, 0.3, 0.1, data->current == 0 ? data->active_button : data->button, NULL);
 	image_render(&data->dev->image, 0.35, 0.3, 0.3, 0.1, data->current == 1 ? data->active_button : data->button, NULL);
@@ -92,8 +102,6 @@ void homemode_draw(ESContext* esContext, State* state)
 	color.v[2] = 0.0;
 	text_draw(data->current == 0 ? &data->dev->textbold : &data->dev->textregular, "Start" , 0.46, 0.53, 0.6, &color);
 	text_draw(data->current == 1 ? &data->dev->textbold : &data->dev->textregular, "Exit" , 0.4715, 0.326, 0.6, &color);
-
-	
 }
 
 State homemode_state = {
